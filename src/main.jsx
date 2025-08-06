01a37f33ef389ec30d9887b68564d90410fbdc2a
sulemaan7070 <sulemaan7070@gmail.com>
2025-08-06 07:03:41 +0530

    added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import AppNew from "./AppNew.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <AppNew />
+  </StrictMode>
+);
